277e41df158b42071ef42108591b93369cc3a68e
Clarisse U <clarisseum@gmail.com>
2019-08-25 17:03:31 +0200

    add form module and create class

--- Citations/src/app/app.module.ts
+++ Citations/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { FormCitationComponent } from './form-citation/form-citation.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Citations/src/app/citation.ts
@@ -0,0 +1,5 @@
+export class Citation {
+    quote: string;
+    name: string;
+    author: string;
+}
