076659988d9bb4f55b7932db76f715b3be4ced6a
Clarisse U <clarisseum@gmail.com>
2019-08-25 17:08:38 +0200

    import the class

--- Citations/src/app/form-citation/form-citation.component.ts
+++ Citations/src/app/form-citation/form-citation.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Citation} from '../citation'
 @Component({
   selector: 'app-form-citation',
   templateUrl: './form-citation.component.html',
   styleUrls: ['./form-citation.component.css']
 })
 export class FormCitationComponent implements OnInit {
-
+  newCitation:Citation[];
   constructor() { }
 
   ngOnInit() {
