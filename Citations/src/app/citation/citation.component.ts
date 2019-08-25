30879a48c8942b3806a7bbd3239da5e93ceae476
Clarisse U <clarisseum@gmail.com>
2019-08-25 17:26:58 +0200

    add the output and the event emitter

--- Citations/src/app/citation/citation.component.ts
+++ Citations/src/app/citation/citation.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Citation } from '../citation'
 @Component({
   selector: 'app-citation',
   templateUrl: './citation.component.html',
   styleUrls: ['./citation.component.css']
 })
 export class CitationComponent implements OnInit {
+  newCitation:Citation[];
 
   constructor() { }
 
--- Citations/src/app/form-citation/form-citation.component.ts
+++ Citations/src/app/form-citation/form-citation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
 import {Citation} from '../citation'
 @Component({
   selector: 'app-form-citation',
@@ -6,7 +6,13 @@ import {Citation} from '../citation'
   styleUrls: ['./form-citation.component.css']
 })
 export class FormCitationComponent implements OnInit {
-  newCitation:Citation[];
+  newCitation = new Citation();
+  @Output() addCitation = new EventEmitter<Citation>();
+  submitCit(){
+    this.addCitation.emit (this.newCitation);
+    this.newCitation = new Citation();
+  }
+
   constructor() { }
 
   ngOnInit() {
